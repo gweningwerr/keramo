5170de96ca087442c150cfc070cfcce811e5d7f8
mshirnin <mshirnin@treto.ru>
2016-08-31 17:06:06 +0700

    add UserController

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,14 @@
+<?php
+namespace AppBundle\Controller;
+/**
+ * class: UserController
+ * -----------------------------------------------------
+ * Created by MihailShirnin on 31.08.2016.
+ * @package AppBundle\Controller
+ */
+
+class UserController extends ExtendedController
+{
+
+}
+
