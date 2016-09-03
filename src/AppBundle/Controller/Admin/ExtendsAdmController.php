05c65c27631795964c1127ddb2231faff7e92b6c
mshirnin <mshirnin@treto.ru>
2016-09-03 22:58:40 +0700

    добавил ExtendsAdmController на всякий случай

---
+++ src/AppBundle/Controller/Admin/ExtendsAdmController.php
@@ -0,0 +1,17 @@
+<?php
+namespace AppBundle\Controller\Admin;
+use AppBundle\Controller\ExtendedController;
+
+
+/**
+ * class: ExtendsAdmController
+ * -----------------------------------------------------
+ * Created by MihailShirnin on 02.09.2016.
+ * @package AppBundle\Controller\Admin
+ */
+
+class ExtendsAdmController extends ExtendedController
+{
+
+}
+
