fc5d882841325e06ab929037431e89056dba087a
mshirnin <mshirnin@treto.ru>
2016-09-05 04:57:37 +0700

    add select2

--- src/AppBundle/Resources/public/js/app.js
+++ src/AppBundle/Resources/public/js/app.js
@@ -8,3 +8,11 @@
 function isUrl(href) {
 	return window.location.href.indexOf(href) !== -1;
 }
+$(document).ready(function () {
+	//$.fn.select2.defaults.set( "theme", "bootstrap" );
+	$( ".select2" ).select2({
+		theme: "bootstrap",
+		allowClear: true,
+		width: '100%'
+	});
+});
