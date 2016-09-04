442c8175866e0fbfc9880555e6796ff5a5f093e2
mshirnin <mshirnin@treto.ru>
2016-09-05 05:02:46 +0700

    upd список и редактирование

--- src/AppBundle/Controller/Admin/UsersAdmController.php
+++ src/AppBundle/Controller/Admin/UsersAdmController.php
@@ -4,7 +4,9 @@ use AppBundle\Entity\UserEntity;
 use AppBundle\Helper\App;
 use AppBundle\Helper\RequestHelper;
 use AppBundle\Repository\UserRepository;
+use DateTime;
 use Doctrine\Common\Persistence\ObjectRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 /**
@@ -19,43 +21,76 @@ class UsersAdmController extends ExtendsAdmController
 	private $repoUser;
 
 
+	/**
+	 * Список юзеров
+	 * @return null|\Symfony\Component\HttpFoundation\JsonResponse
+	 */
 	public function listAction()
 	{
 		if (RequestHelper::isAjax()) {
-			$users = $this->repoUser()->findAll();
-
-			$rows = [];
-			/** @var UserEntity $user  */
-			foreach ($users as $user) {
-				$rows[] = [
-					'id' => $user->getId(),
-					'name' => $user->getUsername(),
-					'stargazers_count' => 20,
-					'forks_count' => 122,
-					'description' => 'description',
-					'links' => [
-						'edit' => $this->generateUrl('adminka.user.edit', ['id'=>$user->getId()]),
-						'delete' => $this->generateUrl('adminka.user.delete', ['id'=>$user->getId()]),
-					],
-					'actions' => [
-						'delete' => $this->generateUrl('adminka.user.delete', ['id'=>$user->getId()]),
-						'edit' => $this->generateUrl('adminka.user.edit', ['id'=>$user->getId()]),
-					],
-				];
-			}
-			$response = ['rows' => $rows, "total"=> count($rows),];
+			$response = $this->getListUsersBySorted();
 		} else {
 			$response = $this->render('AppBundle:Adminka:user_list.html.twig', [
-				'sendUrl' => $this->generateUrl('adminka.user.list'),
+				'sendUrl' => $this->generateUrl('adminka.user.list')
 			]);
 		}
 
 		return $this->ok($response);
 	}
 
-	public function editAction()
+	private function getListUsersBySorted()
 	{
-		App::dumpExit('UsersAdmController:editAction');
+		$bsTable = RequestHelper::getVar('bstable', []);
+		$users = $this->repoUser()->getByBsTable($bsTable);
+
+
+		$rows = [];
+		/** @var UserEntity $user  */
+		foreach ($users as $user) {
+			$lastLogin = $user->getLastLogin() ? $user->getLastLogin()->format('m.d.Y / H:i') : null;
+			$rows[] = [
+				'id' => $user->getId(),
+				'username' => $user->getUsername(),
+				'email' => $user->getEmail(),
+				'lastLogin' => $lastLogin,
+				'links' => [
+					'edit' => $this->generateUrl('adminka.user.edit', ['id'=>$user->getId()]),
+					'delete' => $this->generateUrl('adminka.user.delete', ['id'=>$user->getId()]),
+				],
+				'actions' => [
+					'delete' => $this->generateUrl('adminka.user.delete', ['id'=>$user->getId()]),
+					'edit' => $this->generateUrl('adminka.user.edit', ['id'=>$user->getId()]),
+				],
+			];
+		}
+		$response = ['rows' => $rows, "total"=> count($rows)];
+		return $response;
+	}
+
+	public function editAction($id)
+	{
+		/** @var UserEntity $user  */
+		$user = $this->repoUser()->find($id);
+
+//		$securityContext = App::getContainer()->get('security.authorization_checker');
+//		$res = $securityContext->isGranted('ROLE_SUPER_ADMIN', $user);
+//
+//		App::dump($res, App::getContainer()->getParameter('security.role_hierarchy.roles') );
+//
+//
+//		App::dump($user->getGroups());
+//		App::dumpExit();
+		if (!$user) {
+			throw new NotFoundHttpException('Такого пользователя нет');
+		}
+		$response = $this->render('AppBundle:Adminka:user_edit.html.twig', [
+			'user' => $user,
+			'roles' => App::getListRoles(),
+			'formAction' => $this->generateUrl('adminka.user.update', ['id'=>$user->getId()]),
+			'linkList' => $this->generateUrl('adminka.user.list'),
+		]);
+
+		return $this->ok($response);
 	}
 
 	public function deleteAction()
@@ -63,9 +98,49 @@ class UsersAdmController extends ExtendsAdmController
 		App::dumpExit('UsersAdmController:deleteAction');
 	}
 
-	public function updateAction()
+	public function updateAction($id)
 	{
-		App::dumpExit('UsersAdmController:updateAction');
+		/** @var UserEntity $user  */
+		$user = $this->repoUser()->find($id);
+		if (!$user) {
+			return $this->error('Такого пользователя нет');
+		}
+
+		$newPwd = RequestHelper::getString('new_password');
+		if ($newPwd) {
+			$encoder = $this->container->get('security.encoder_factory')->getEncoder($user);
+			if (!$encoder) {
+				return $this->error('Ошибка получения енкодера');
+			}
+			$newPwdEncoded = $encoder->encodePassword($newPwd, $user->getSalt());
+			$user->setPasswordRequestedAt(new DateTime);
+			$user->setPassword($newPwdEncoded);
+		}
+
+		$email = RequestHelper::getEmail('email');
+		if ($email) {
+			$user->setEmail($email);
+			$user->setEmailCanonical($email);
+		}
+
+		$roles = RequestHelper::getVar('roles');
+		if ($roles && is_array($roles)) {
+			$user->setRoles($roles);
+		}
+
+		App::em()->persist($user);
+		App::em()->flush();
+
+		$response = [
+			'Request' => RequestHelper::getAll(),
+			'msg' => 'Данные были успешно обновлены. Выберете дальнейшее действие.',
+			'$roles' => $roles,
+			'link' => [
+				'list' => $this->generateUrl('adminka.user.list'),
+				'edit' => $this->generateUrl('adminka.user.edit', ['id'=>$user->getId()]),
+			],
+		];
+		return $this->ok($response);
 	}
 
 	/**
